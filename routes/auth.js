6824613550e507ea26a8d310b99fa5f74cd71dc4
sverma507 <sverma70568@gmail.com>
2024-09-23 10:48:14 +0530

    new

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { signup, login, adminLogin, forgotPasswordController } = require('../controllers/authController');
+const { signUp, login, adminLogin, forgotPasswordController } = require('../controllers/authController');
 const { loginMiddleware, adminProtect } = require('../middleware/auth');
 
-router.post('/signup', signup);
+router.post('/signup', signUp);
 router.post('/login', login);
 router.get('/admin-auth', adminProtect,(req,res) => {res.status(200).send({ok:true})});
 router.post('/forgot-password', forgotPasswordController)
