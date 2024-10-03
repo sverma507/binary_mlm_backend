5365a93be1b51e13eb59edf8e5268bbf37b9395c
sverma507 <sverma70568@gmail.com>
2024-10-03 10:53:38 +0530

    new

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ dotenv.config();
 const app = express();
 app.use(cors(
   {
-    // origin: 'https://www.utechtrading.com',
-    origin: 'http://localhost:3000',
+    origin: 'https://www.utechtrading.com',
+    // origin: 'http://localhost:3000',
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     credentials: true
   }
