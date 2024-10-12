3caf1a86eec4419fd30d4b260006acc433e7ae59
sverma507 <sverma70568@gmail.com>
2024-10-12 14:58:29 +0530

    new

--- server.js
+++ server.js
@@ -16,14 +16,13 @@ dotenv.config();
 
 
 const app = express();
-app.use(cors(
-  {
-    origin: 'https://www.utechtrading.com',
-    // origin: 'http://localhost:3000',
-    methods: 'GET,POST,PUT,DELETE,OPTIONS',
-    credentials: true
-  }
-));
+app.use(cors({
+  origin: ['https://www.utechtrading.com', 'http://localhost:3000'], // Allow both the production and local origins
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  credentials: true, // This allows credentials such as cookies to be sent
+  optionsSuccessStatus: 200 // Some legacy browsers choke on 204
+}));
+
 
 app.use(express.json());
 app.use(fileUpload({
