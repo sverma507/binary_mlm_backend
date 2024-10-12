d7ec847bbb7758f9e7472e40ce05b516d28552ba
sverma507 <sverma70568@gmail.com>
2024-10-12 15:25:01 +0530

    new

--- server.js
+++ server.js
@@ -10,14 +10,13 @@ const payoutRoutes = require('./routes/payout')
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
 const { calculateLegBusinessPerLevel } = require('./controllers/business');
-
 dotenv.config();
 
 
 
 const app = express();
 app.use(cors({
-  origin: '*', // Allow both the production and local origins
+  origin: 'https://www.utechtrading.com', // Allow both the production and local origins
   methods: 'GET,POST,PUT,DELETE,OPTIONS',
   credentials: true, // This allows credentials such as cookies to be sent
   optionsSuccessStatus: 200 // Some legacy browsers choke on 204
