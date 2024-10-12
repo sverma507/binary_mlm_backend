109b0f4a0130e7df5cb7656f07f9ceca61b099a9
sverma507 <sverma70568@gmail.com>
2024-10-12 15:13:12 +0530

    new

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ dotenv.config();
 
 
 const app = express();
-// app.use(cors({
-//   origin: ['https://www.utechtrading.com', 'http://localhost:3000'], // Allow both the production and local origins
-//   methods: 'GET,POST,PUT,DELETE,OPTIONS',
-//   credentials: true, // This allows credentials such as cookies to be sent
-//   optionsSuccessStatus: 200 // Some legacy browsers choke on 204
-// }));
+app.use(cors({
+  origin: '*', // Allow both the production and local origins
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  credentials: true, // This allows credentials such as cookies to be sent
+  optionsSuccessStatus: 200 // Some legacy browsers choke on 204
+}));
 
 
 app.use(express.json());
@@ -31,9 +31,7 @@ app.use(fileUpload({
 }));
 
 
-
-
-app.options('*', cors()); // Include this line to respond to preflight requests
+// Include this line to respond to preflight requests
 
 
 // Connect to MongoDB
